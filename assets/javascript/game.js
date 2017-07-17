4f09967208bff7eebae43d3cdbb7096447863e5b
stanley <chw058@gmail.com>
2017-07-16 17:41:04 -0700

    bug & minor fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+	//setting up the necessary variables
 	var fightImgUrlArray = ["assets/images/blastoise.jpg", "assets/images/charizard.jpg", "assets/images/pidgeot.jpg", "assets/images/venusaur.jpg"];
 	var fighterNameArray = ["blastoise", "charizard", "pidgeot", "venusaur"];
 	var opponentNameArray = [];
@@ -7,17 +8,16 @@ $(document).ready(function() {
 	var isDefeated = false;
 	var player = null;
 	var opponent = null;
-	var playerAttack = 0;
-	var originalPage = $("body").html();
-	//console.log(originalPage);
+	var originalPage = $("body").html(); //might have better wy to do this
 
+	//hide some elements
 	$("#attack").hide();
 	$("#restart").hide();
 	$("#yourFighter").hide();
 	$("#currFighting").hide();
 	$("#opponentList").hide();
 
-
+	//this function resets the entire game to the initial state
 	function reset() {
 
 		$("body").html(originalPage);
@@ -29,18 +29,12 @@ $(document).ready(function() {
 		isDefeated = false;
 		player = null;
 		opponent = null;
-		playerAttack = 0;
+		//playerAttack = 0;
 
 		blastoiseObj = new Fighter("Blastoise", 170, 8, 5, fightImgUrlArray[0]);
 		charizardObj = new Fighter("Charizard", 100, 6, 5, fightImgUrlArray[1]);
 		pidgeotObj = new Fighter("Pidgeot", 150, 7, 20, fightImgUrlArray[2]);
 		venusaurObj = new Fighter("Venusaur", 180, 8, 25, fightImgUrlArray[3]);
-
-	//testing
-	//console.log(blastoiseObj.hp);
-	//console.log(charizardObj.hp);
-	//console.log(pidgeotObj.hp);
-	//console.log(venusaurObj.hp);
 
 		fighterObj = {
 
@@ -51,16 +45,19 @@ $(document).ready(function() {
 
 		};
 
+		//setup fighter's name display
 		$("#charOneName").html(fighterObj.blastoise.name);
 		$("#charTwoName").html(fighterObj.charizard.name);
 		$("#charThreeName").html(fighterObj.pidgeot.name);
 		$("#charFourName").html(fighterObj.venusaur.name);
 
+		//setup fighter's HP display
 		$("#charOneHP").html("HP: " + fighterObj.blastoise.hp);
 		$("#charTwoHP").html("HP: " + fighterObj.charizard.hp);
 		$("#charThreeHP").html("HP: " + fighterObj.pidgeot.hp);
 		$("#charFourHP").html("HP: " + fighterObj.venusaur.hp);
 
+		//hide elements not used in initial state
 		$("#attack").hide();
 		$("#restart").hide();
 		$("#yourFighter").hide();
@@ -79,7 +76,6 @@ $(document).ready(function() {
 		this.baseAttack = attackPoint;
 		this.counter = counterAttackPoint;
 		this.imageUrl = imgUrl;
-		//this.player = false;
 
 	}
 
@@ -89,12 +85,7 @@ $(document).ready(function() {
 	var pidgeotObj = new Fighter("Pidgeot", 150, 7, 20, fightImgUrlArray[2]);
 	var venusaurObj = new Fighter("Venusaur", 180, 8, 25, fightImgUrlArray[3]);
 
-	//testing
-	//console.log(blastoiseObj.hp);
-	//console.log(charizardObj.hp);
-	//console.log(pidgeotObj.hp);
-	//console.log(venusaurObj.hp);
-
+	//place fighter object in another object for retrieval/processing
 	fighterObj = {
 
 		blastoise: blastoiseObj,
@@ -104,51 +95,46 @@ $(document).ready(function() {
 
 	};
 
+	//setup fighter's name display
 	$("#charOneName").html(fighterObj.blastoise.name);
 	$("#charTwoName").html(fighterObj.charizard.name);
 	$("#charThreeName").html(fighterObj.pidgeot.name);
 	$("#charFourName").html(fighterObj.venusaur.name);
 
+	//setup fighter's HP display
 	$("#charOneHP").html("HP: " + fighterObj.blastoise.hp);
 	$("#charTwoHP").html("HP: " + fighterObj.charizard.hp);
 	$("#charThreeHP").html("HP: " + fighterObj.pidgeot.hp);
 	$("#charFourHP").html("HP: " + fighterObj.venusaur.hp);
 
+	//when a fighter is selected, move the selected fighter to #yourFighter section and the remainder to #opponetList section
 	$(".fighter").click(function(){
 
-		//console.log($(this).attr("id"));
 		var selectedName = $(this).attr("id");
-		//console.log(temp);
-		//console.log(fighterObj[temp].imageUrl);
-
-		//moved elected pokemon to your fighter
-		/*
-		var img = $("<img>");
-		img.attr("src", fighterObj[selectedName].imageUrl);
-		img.attr("class", "fighter");
-		$("#yourFighter").append(img);
-		*/
 
 		movePlayer(selectedName, "#yourFighter");
-
 		displayNameHP(selectedName, "#yourFighter");
 
+		//set player to be th object that was selected
 		player = fighterObj[selectedName];
-		playerAttack = player.attack;
+		//playerAttack = player.attack;
+
 		console.log("player is: " + player);
 
-		//console.log(fight)
 		moveOpponet(selectedName);
 
+		//remove the selection screen/instruction
 		eraseRow("#fighterRow1");
 		eraseRow("#fighterRow2");
 
+		//show the necessary elements for the game to run
 		$("#yourFighter").show();
 		$("#currFighting").show();
 		$("#opponentList").show();
 
 	});
 
+	//create the iamge and append it to the parameter named id
 	function movePlayer(charId, id) {
 
 			var img = $("<img>");
@@ -159,11 +145,11 @@ $(document).ready(function() {
 
 	}
 
-
+	//create name and HP right underneath fighter img
 	function displayNameHP(stringName, appendTo) {
 
 		var p = $("<p>");
-		p.html(stringName);
+		p.html(fighterObj[stringName].name);
 		p.attr("id", "charName" + stringName);
 		$(appendTo).first().append(p);
 		p = $("<p>");
@@ -174,9 +160,10 @@ $(document).ready(function() {
 
 	} 
 
+	//move the unselected fighters to #opponentList
 	function moveOpponet(objName) {
 		
-		//make copy of fighterNameArray, find htee element to be removed
+		//make copy of fighterNameArray, find the element to be removed
 		var tempArray = fighterNameArray.slice();
 		var index = tempArray.indexOf(objName);
 
@@ -189,29 +176,20 @@ $(document).ready(function() {
 
 		//create a list of oponent names
 		opponentNameArray = tempArray;
-		//place the remaining array, which is a list of opponents, and move them into the img tag of OpponentList column
-		
-		//var element = $("#opponentList").children().next();
-		console.log(tempArray);
+
+		//move the fighters to the correct place inside #opponentList
 		var element = $("#opponentList div");
-		//var counter = 0;
+		
 		for (var i = 0; i < tempArray.length; i++) {
 
-			//var child = ".next()";
-			//var tempElement = element.next();
-
 			if (tempArray[i] !== objName) {
-
-				
-				//console.log(tempArray[i]);
-				//console.log(fighterObj[tempArray[i]].imageUrl);
-
 
 				movePlayer(tempArray[i], element);
 
 				displayNameHP(tempArray[i], element);			
 
 			};
+
 			element = element.next();
 	
 		};
@@ -220,24 +198,28 @@ $(document).ready(function() {
 
 	};
 
+	//Used to remove the fighter selection rows
 	function eraseRow(idRow) {
 
 		$(idRow).html("");
 
 	}
 
+	//When opponent inside #opponentList is clicked, move that fighter to #currFighting
 	$(".opponent").click(function(){
 
+		//if a round is already underway, dont do anything, otherwise, move fighter and start round by setting isPlayng to true
 		if (!isPlaying) {
 
-		var selectedName = $(this).children().attr("id");
-		//$("#defender").attr("id", selectedName);
-		console.log(selectedName);
-		//moveDefender(selectedName);
-		movePlayer(selectedName, "#currFighting");
-		displayNameHP(selectedName, "#currFighting");
-		updateDefender(selectedName);
-		$(this).html("");
+			var selectedName = $(this).children().attr("id");
+			
+			
+			movePlayer(selectedName, "#currFighting");
+			displayNameHP(selectedName, "#currFighting");
+			updateDefender(selectedName);
+
+			//remove the fighter from #opponentList
+			$(this).html("");
 
 		}
 
@@ -245,6 +227,7 @@ $(document).ready(function() {
 
 	});
 
+	//remove a fighter from opponentNameArray and set opponent to the removed fighter
 	function updateDefender(objName){
 
 		var index = opponentNameArray.indexOf(objName);
@@ -255,36 +238,37 @@ $(document).ready(function() {
 			
 		};
 
-		console.log(opponentNameArray);
-
-
-		
 		opponent = fighterObj[objName];
-		//console.log(opponent);
-		//isPlaying = true;
 
 	};
 
+	//main meat of the program. Handle logic of the game when attack button is clicked
 	$("#attack").click(function(){
 
+		// When attack button is clicked, check if the game is playing and the player is still alive
 		if (isPlaying && !isDefeated) {
-		//$("#result").html("hi");
-
-		//attack(player,opponent);
+			
+			//update HP to reflect the attack
 			updateHP(player, opponent);
 			
+			//display fighter action
 			displayResultText(player, opponent);
+
+			//update player attack power as per requirement
 			updateAttack(player);
+
+			//check if a round has ended
 			if (checkRoundEnd(player,opponent)) {
 
+				//if opponent is still alive at game end, player is defeated, and this case is handled by displayTextResult
 				if (opponent.hp >= 0) {
 
 				} else {
-				
+					
+					//remove the currFIghter, so user can select a new one.
 					removeOpponent();
 
-					console.log(opponentNameArray.length);
-
+					//When all opponent are removed, declare win and show reset button. This case handles when user defeats all opponents for the frist time.
 					if (opponentNameArray.length === 0) {
 
 						$("#resultOne").html("You have vanquished all foes!");
@@ -299,14 +283,9 @@ $(document).ready(function() {
 			}
 
 
-		}  
+		}  else {
 
-		else {
-
-			console.log("round should end");
-			//remove opponent from defender...
-			//removeOpponent();
-
+			//This case handles if user click on attack button at the end of the game. When user isnt playing (isPlaying = false), but is not deeated, and user keeps on clicking attack button
 			if (opponentNameArray.length === 0) {
 
 				$("#resultOne").html("You have vanquished all foes!");
@@ -314,9 +293,9 @@ $(document).ready(function() {
 				$("#restart").show();
 
 			} else if (isDefeated) {
-
-			} else{
-
+				//if defeated, dont do anything, this case is taken care of by displayResultText
+			} else {
+				//tell the player to select an opponent
 				$("#resultOne").html("There is no one to fight!");
 				$("#resultTwo").html("Pick an opponent");
 
@@ -325,10 +304,13 @@ $(document).ready(function() {
 
 	});
 
+	//display text of three kind. 
+	//1st: player defeated.
+	//2nd: player win, but has more opponent to fight
+	//3rd: attack process
 	function displayResultText(playerObj, opponentObj) {
 
 		var lineOne;
-
 		var lineTwo;
 
 		console.log("player HP:" + playerObj.hp);
@@ -341,9 +323,6 @@ $(document).ready(function() {
 			isDefeated = true;
 			$("#restart").show();
 
-			//show reset button
-
-
 		} else if (opponentObj.hp <= 0 ) {
 
 			lineOne = "Congratulation! You have defeated your enemy!!!"
@@ -352,29 +331,24 @@ $(document).ready(function() {
 
 		} else {
 
-		lineOne = playerObj.name + " attacks " + opponentObj.name + " for " + playerObj.attack + " damage.";
+			lineOne = playerObj.name + " attacks " + opponentObj.name + " for " + playerObj.attack + " damage.";
 
-		lineTwo = opponentObj.name + " counter attacks " + playerObj.name + " for " + opponentObj.counter + " damage.";
+			lineTwo = opponentObj.name + " counter attacks " + playerObj.name + " for " + opponentObj.counter + " damage.";
 
 		}
 
 		$("#resultOne").html(lineOne);
 		$("#resultTwo").html(lineTwo);
 
-
-
 	}
 
+	//update player and opponent HP
 	function updateHP(playerObj, opponentObj) {
 
 		var playerHPSelector = "#charHP" + playerObj.name.toLowerCase();
 		var opponentHPSelector = "#charHP" + opponentObj.name.toLowerCase();
 
 		opponentObj.hp = opponentObj.hp - playerObj.attack;
-		//console.log(opponentObj.hp);
-
-		//console.log(playerObj.name);
-		//console.log(playerObj.hp);
 		playerObj.hp = playerObj.hp - opponentObj.counter;
 
 		$(playerHPSelector).html(playerObj.hp);
@@ -382,12 +356,14 @@ $(document).ready(function() {
 
 	}
 
+	//update Player's attack power
 	function updateAttack(playerObj) {
 
 		playerObj.attack = playerObj.attack + playerObj.baseAttack;
 
 	}
 
+	//check if the round has ended
 	function checkRoundEnd(playerObj, opponentObj) {
 
 		if(playerObj.hp <= 0 || opponentObj.hp <=0) {
@@ -400,9 +376,9 @@ $(document).ready(function() {
 
 		}
 
-
 	}
 
+	//remove opponent from #currFghting section
 	function removeOpponent() {
 
 		$("#currFighting").html("");
@@ -410,6 +386,7 @@ $(document).ready(function() {
 
 	}
 
+	//restarts the entire game
 	$("#restart").click(function(){
 
 		console.log("clicked on reset");
